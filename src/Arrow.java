2985fe15d46e29df0b9d5138c4b7ff9c5cc481b8
arincdemir <68223368+arincdemir@users.noreply.github.com>
2022-04-19 16:52:11 +0300

    formatted files

--- src/Arrow.java
+++ src/Arrow.java
@@ -1,30 +1,24 @@
 
 public class Arrow {
-	
 
-	static Vector2D[] verticesRelativeToUnitVectors = {new Vector2D(-0.2, -1), new Vector2D(0.2, -1), new Vector2D(0.2, 1), new Vector2D(0.3, 1) ,
-			 new Vector2D(0, 1.5), new Vector2D(-0.3, 1), new Vector2D(-0.2, 1)};
-	
+	static Vector2D[] verticesRelativeToUnitVectors = { new Vector2D(-0.2, -1), new Vector2D(0.2, -1),
+			new Vector2D(0.2, 1), new Vector2D(0.3, 1), new Vector2D(0, 1.5), new Vector2D(-0.3, 1),
+			new Vector2D(-0.2, 1) };
+
 	private Vector2D[] vertices = new Vector2D[verticesRelativeToUnitVectors.length];
 	private double[] xOfVertices = new double[vertices.length];
 	private double[] yOfVertices = new double[vertices.length];
 
-	
-	
 	private double scale;
 	private Vector2D midPoint;
-	
-	
+
 	public Vector2D unitVector;
 	public Vector2D perpendicularVector;
 
-	
-	
-	
 	public Arrow() {
 		this(0.5, 0.5, 0.1, new Vector2D(0.707, 0.707));
 	}
-	
+
 	public Arrow(double x, double y, double scale, Vector2D unitVector) {
 		midPoint = new Vector2D(x, y);
 		this.scale = scale;
@@ -32,29 +26,31 @@ public class Arrow {
 		this.perpendicularVector = new Vector2D(unitVector.y, -unitVector.x);
 		createVertices();
 	}
-	
+
 	private void createVertices() {
 		for (int i = 0; i < verticesRelativeToUnitVectors.length; i++) {
-			double x = verticesRelativeToUnitVectors[i].x * perpendicularVector.x + verticesRelativeToUnitVectors[i].y * unitVector.x ;
-			double y = verticesRelativeToUnitVectors[i].x * perpendicularVector.y + verticesRelativeToUnitVectors[i].y * unitVector.y ;
+			double x = verticesRelativeToUnitVectors[i].x * perpendicularVector.x
+					+ verticesRelativeToUnitVectors[i].y * unitVector.x;
+			double y = verticesRelativeToUnitVectors[i].x * perpendicularVector.y
+					+ verticesRelativeToUnitVectors[i].y * unitVector.y;
 			vertices[i] = Vector2D.addVectors(midPoint, Vector2D.scaleVector(new Vector2D(x, y), scale));
 		}
-		
+
 		for (int i = 0; i < vertices.length; i++) {
 			xOfVertices[i] = vertices[i].x;
 			yOfVertices[i] = vertices[i].y;
 
 		}
 	}
-	
+
 	public void draw() {
 		StdDraw.filledPolygon(xOfVertices, yOfVertices);
 	}
-	
+
 	public double getX() {
 		return midPoint.x;
 	}
-	
+
 	public double getY() {
 		return midPoint.y;
 	}
--- src/Main.java
+++ src/Main.java
@@ -5,13 +5,13 @@ public class Main {
 	static final int CANVAS_HEIGTH = 512;
 	static final int FPS = 30;
 	static final double MS_PER_FRAME = 1000 / FPS;
-	
+
 	static Color bgColor = Color.black;
 	static boolean running = true;
 	static double previousTime = 0;
 	static double deltaTime = 0;
 	static Arrow arrow1 = new Arrow();
-	
+
 	public static void main(String[] args) {
 		StdDraw.setCanvasSize(CANVAS_WIDTH, CANVAS_HEIGTH);
 		StdDraw.enableDoubleBuffering();
@@ -19,33 +19,31 @@ public class Main {
 		initializeStuff();
 		mainLoop();
 	}
-	
+
 	private static void initializeStuff() {
-		
+
 		previousTime = System.currentTimeMillis();
 	}
-	
+
 	private static void mainLoop() {
 		while (running) {
 			StdDraw.clear(bgColor);
-			
+
 			System.out.println("1");
 			arrow1.draw();
-			
-			
-			
+
 			StdDraw.show();
 			waitForNextFrame();
 		}
 	}
-	
+
 	private static void waitForNextFrame() {
 		do {
 			deltaTime += System.currentTimeMillis() - previousTime;
 			previousTime = System.currentTimeMillis();
 			StdDraw.pause(1);
 		} while (deltaTime <= MS_PER_FRAME);
-		
+
 		deltaTime -= MS_PER_FRAME;
 	}
 
