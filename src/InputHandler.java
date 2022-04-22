c899b0ad41a13ee994e0cd8e6ac6865b5380dc55
arincdemir <68223368+arincdemir@users.noreply.github.com>
2022-04-22 16:02:49 +0300

    done, now it you can add and delete wires with a and d

--- src/InputHandler.java
+++ src/InputHandler.java
@@ -22,10 +22,24 @@ public class InputHandler {
 			if (key == addKey) {
 				addKeyPressed();
 			} else if (key == delKey) {
-
+				delKeyPressed();
 			}
 		}
 
+	}
+
+	private static void delKeyPressed() {
+		Vector2D mousePos = new Vector2D(StdDraw.mouseX(), StdDraw.mouseY());
+		
+		SourceWire wireToRemove = null;
+	
+		for (SourceWire sourceWire : sourceWires) {
+			if (Vector2D.substractVectors(sourceWire, mousePos).magnitude <= SourceWire.radius) {
+				wireToRemove = sourceWire;
+			}
+		}
+			sourceWires.remove(wireToRemove);
+		
 	}
 
 	private static void addKeyPressed() {
